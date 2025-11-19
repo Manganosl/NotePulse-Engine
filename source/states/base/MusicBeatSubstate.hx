f79f3f120299b197114c97cf63006959afc8d9f4
Manganos <elprotazo1@gmail.com>
2025-11-19 17:17:25 +0100

    Global runtimeShaders or smth

--- source/states/base/MusicBeatSubstate.hx
+++ source/states/base/MusicBeatSubstate.hx
@@ -3,14 +3,15 @@ package states.base;
 import flixel.FlxSubState;
 import psychlua.HScript;
 import psychlua.LuaUtils;
-import psychlua.FunkinLua;
+#if !flash
+import flixel.addons.display.FlxRuntimeShader;
+#end
 
 class MusicBeatSubstate extends FlxSubState
 {
-	public function new()
-	{
-		super();
-	}
+	#if (!flash && sys)
+	public var runtimeShaders:Map<String, Array<String>> = new Map<String, Array<String>>();
+	#end
 
 	#if (LUA_ALLOWED || HSCRIPT_ALLOWED)
 	public function addTextToDebug(text:String, color:FlxColor, ?trace:Bool = false, ?type:String) {
@@ -234,4 +235,73 @@ class MusicBeatSubstate extends FlxSubState
 		if(PlayState.SONG != null && PlayState.SONG.notes[curSection] != null) val = PlayState.SONG.notes[curSection].sectionBeats;
 		return val == null ? 4 : val;
 	}
+
+	#if (!flash && sys)
+	public function createRuntimeShader(name:String):FlxRuntimeShader
+	{
+		if(!ClientPrefs.data.shaders) return new FlxRuntimeShader();
+
+		#if (!flash && MODS_ALLOWED && sys)
+		if(!runtimeShaders.exists(name) && !initLuaShader(name))
+		{
+			Log.error('Shader $name is missing!');
+			return new FlxRuntimeShader();
+		}
+
+		var arr:Array<String> = runtimeShaders.get(name);
+		var daShader:FlxRuntimeShader = new FlxRuntimeShader(arr[0], arr[1]);
+		return daShader;
+		#else
+		Log.error("Platform unsupported for Runtime Shaders!");
+		return null;
+		#end
+	}
+
+	public function initLuaShader(name:String, ?glslVersion:Int = 120)
+	{
+		if(!ClientPrefs.data.shaders) return false;
+
+		#if (MODS_ALLOWED && !flash && sys)
+		if(runtimeShaders.exists(name))
+		{
+			Log.hxTrace('Shader $name was already initialized!');
+			return true;
+		}
+
+		for (folder in Mods.directoriesWithFile(Paths.getSharedPath(), 'shaders/'))
+		{
+			var frag:String = folder + name + '.frag';
+			var vert:String = folder + name + '.vert';
+			var found:Bool = false;
+			if(FileSystem.exists(frag))
+			{
+				frag = File.getContent(frag);
+				found = true;
+			}
+			else frag = null;
+
+			if(FileSystem.exists(vert))
+			{
+				vert = File.getContent(vert);
+				found = true;
+			}
+			else vert = null;
+
+			if(found)
+			{
+				runtimeShaders.set(name, [frag, vert]);
+				return true;
+			}
+		}
+			#if (LUA_ALLOWED || HSCRIPT_ALLOWED)
+			addTextToDebug('Missing shader $name .frag AND .vert files!', FlxColor.RED, true, "error");
+			#else
+			Log.error('Missing shader $name .frag AND .vert files!');
+			#end
+		#else
+		Log.error('This platform doesn\'t support Runtime Shaders!');
+		#end
+		return false;
+	}
+	#end
 }
