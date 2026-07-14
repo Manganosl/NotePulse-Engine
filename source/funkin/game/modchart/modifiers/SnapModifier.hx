011992dfa544adce39c1ec6d99244009b5b74982
Manganosl <elprotazo1@gmail.com>
2026-07-14 23:12:55 +0200

    Fuck Snap

--- source/funkin/game/modchart/modifiers/SnapModifier.hx
+++ source/funkin/game/modchart/modifiers/SnapModifier.hx
@@ -4,7 +4,6 @@ class SnapModifier extends NoteModifier {
 	override function getOrder() return Modifier.ModifierOrder.LAST;
 
 	override function getPos(time:Float, visualDiff:Float, timeDiff:Float, beat:Float, pos:Vector3, data:Int, player:Int, obj:FlxSprite){
-		if(obj is Note && cast(obj, Note).isSustainNote) continue;
 		pos.x = FlxMath.lerp(pos.x, MathUtil.snap(pos.x, getSubmodValue("snapXInterval", player)), getValue(player));
 		pos.y = FlxMath.lerp(pos.y, MathUtil.snap(pos.y, getSubmodValue("snapYInterval", player)), getSubmodValue("snapY", player));
 		pos.z = FlxMath.lerp(pos.z, MathUtil.snap(pos.z, getSubmodValue("snapZInterval", player)), getSubmodValue("snapZ", player));
